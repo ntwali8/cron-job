07c7ffa4909349d66e61134b34e014533bd94c72
Ntwali Raymond <rntwali8@gmail.com>
2021-02-04 12:40:56 +0300

    main

--- cron.php
+++ cron.php
@@ -14,17 +14,19 @@ if($result = $conn->query($sql)) {
 }
 //Opening files depending on the district lists from database
 for ($x = 0; $x < count($districts); $x++) {
-    $districts[$x] .= ".txt";
+    $districts[$x] .= ".txt";//concatinating a .txt extension
 
     $file = fopen($districts[$x], 'r');
-
+    //loading data from file
     while (!feof($file)) {
         $content = fgets($file);
-        $content_array = explode(" ", $content);
-        if ($content_array[0] != "\n") {
+        $content_array = explode(" ", $content);//creating an array for each line in file
+        //check if the array is empty
+        if ($content_array[0] != "") {
+            //set variables to array content
             list($lname,$fname,$date,$gender,$health_condition,$health_officer) = $content_array;
-            $dbinsert = "INSERT INTO 'patient' ('lname', 'fname', 'date', 'gender', 'health_condition', 'health_officer') VALUES ('$lname', '$fname', '$date', '$gender', '$health_condition', '$health_officer')";
-            $result = $conn->query($dbinsert);
+            //create mysql insert query and executes it
+            $result = $conn->query("INSERT INTO `patient`(`lname`, `fname`, `date`, `gender`, `health_condition`, `health_officer`) VALUES ('$lname', '$fname', '$date', '$gender', '$health_condition', '$health_officer')");
             if ($result) {
                 print_r ("Successfully uploaded");
             }
@@ -32,8 +34,10 @@ for ($x = 0; $x < count($districts); $x++) {
                 echo ("Didn't upload");
             }
         }
-        
     }
     fclose($file);
+    //reset file
+    $reset = fopen($districts[$x], 'w');
+    fclose($reset);
 }
 ?>
